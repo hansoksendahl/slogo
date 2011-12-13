5dad2b31e7f225b07c77fced72814adc71d6fdc6
Hans Oksendahl <hansoksendahl@gmail.com>
2011-12-13 08:38:04 -0800

    test

--- public/scripts/slogo.js
+++ public/scripts/slogo.js
@@ -48,7 +48,7 @@ var Slogo = (function () {
     this["container"] = document["getElementById"](container);
     this["renderer"] = new THREE["WebGLRenderer"]();
     this["scene"] = new THREE["Scene"]();
-    this["camera"] = new THREE["Camera"](80, container["offsetWidth"] / container["offsetHeight"], 1, 100);
+    this["camera"] = new THREE["PerspectiveCamera"](80, container["offsetWidth"] / container["offsetHeight"], 1, 100);
     
     this["renderer"]["setSize"](this["container"]["offsetWidth"], this["container"]["offsetHeight"]);
     this["container"]["appendChild"](this["renderer"]["domElement"]);
